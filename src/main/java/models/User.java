95155ce02456f8af797a9da29c56d2d6c5140bb3
friedrice231 <root@Magnus.>
2025-03-14 19:52:43 +0800

    Add User class to model library patrons

---
+++ src/main/java/models/User.java
@@ -0,0 +1,58 @@
+package models;
+
+import java.io.Serializable;
+
+/**
+ * Represents a user in the library system.
+ */
+public class User implements Serializable {
+    protected String userId;
+    protected String name;
+    protected String email;
+    protected String password;
+
+    public User(String userId, String name, String email, String password) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean authenticate(String password) {
+        return this.password.equals(password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
